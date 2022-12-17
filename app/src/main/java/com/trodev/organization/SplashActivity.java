a1176b598f3b4dc480fb7638ab81e6aa947d5170
zobayerdev <zobayer.dev@gmail.com>
2022-12-17 14:59:09 +0600

    One line code added

--- app/src/main/java/com/trodev/organization/SplashActivity.java
+++ app/src/main/java/com/trodev/organization/SplashActivity.java
@@ -18,6 +18,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         }, 2000);
     }
